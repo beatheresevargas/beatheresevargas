ea699cc2bc014b0d932819ecf3013980340fbeb8
Bea Vargas <btpvargas@gmail.com>
2021-03-16 14:59:42 +0800

    Student class

---
+++ Student.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Student {
+	
+	private String StudentID;
+	private int Credits, Points;
+	private double grades;
+	
+	public String getSID()
+	{
+		return StudentID;
+	}
+	
+	public void setSID(String xID)
+	{
+		StudentID = xID;
+	}
+		
+	public int getCreds()
+	{
+		return Credits;
+	}
+	
+	public void setCreds(int xCredits)
+	{
+		Credits = xCredits;
+	}
+	
+	public int getNoOfPoints()
+	{
+		return Points;
+	}
+	
+	public void setNoOfPoints(int xPoints)
+	{
+		Points = xPoints;
+	}
+	
+	public double getGPA()
+	{
+		return grades;
+	}
+	
+	public void setGPA(double xGPA)
+	{
+		grades = xGPA;
+	}
+}

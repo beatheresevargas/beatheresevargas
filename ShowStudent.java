a7bda6c342d5b64ab375a73987e74080863d5329
alcircosas <s1920561@usls.edu.ph>
2021-03-28 01:26:54 +0800

    Modified code for exam

--- ShowStudent.java
+++ ShowStudent.java
@@ -9,7 +9,7 @@ public class ShowStudent {
 		Scanner sc = new Scanner (System.in);
 		
 		System.out.println("Enter your Student ID: ");
-		String StudentID = sc.next();
+		String IDNum = sc.next();
 		
 		System.out.println("Enter the number of credits: ");
 		int Credits = sc.nextInt();
@@ -17,7 +17,7 @@ public class ShowStudent {
 		System.out.println("Enter the number of points: ");
 		int Points= sc.nextInt();
 		
-		Student a = new Student(StudentID, Credits, Points);
+		Student a = new Student(IDNum, Credits, Points);
 		System.out.println(a);
 	}
 	
